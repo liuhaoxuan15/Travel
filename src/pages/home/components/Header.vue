e20e22c15a8afb301fdacdc3af3fd1c0d91a4c38
liuhaoxuan15 <703929634@qq.com>
2018-12-10 18:31:13 +0800

    加入border.css和修改styles快捷路径

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -21,10 +21,11 @@ export default{
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl';
   .header
     display : flex
     line-height: .86rem
-    background: #00bcd4
+    background: $bgColor
     color: #ffffff
     .header-left
       width: .64rem
@@ -44,5 +45,7 @@ export default{
     .header-right
       width: 1.24rem
       float: right
-      margin-left : .2rem
+      text-align : center
+      .city
+        font-size : .2rem
 </style>
