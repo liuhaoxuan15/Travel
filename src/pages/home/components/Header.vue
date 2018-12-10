9d740253cba9cd8abec93e0bb04007260bbc5dfa
liuhaoxuan <liuhaoxuan@cascadiacorp.com>
2018-12-10 15:00:16 +0800

    样式

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        <div class="header-left">返回</div>
+        <div class="header-input">输入城市/景点/游玩/主题</div>
+        <div class="header-righ">城市</div>
+    </div>
+</template>
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+.header
+  display: flex
+  line-height: .86rem
+  background: #00bcd4
+  color: #ffffff
+  .header-left
+    width: .64rem
+    float: left
+  .header-input
+    flex: 1
+    width: 5rem
+    margin-top: .12rem
+    line-height: .64rem
+    height: .64rem
+    margin-left: .2rem
+    background: #ffffff
+    border-radius: .1rem
+    color: #cccccc
+  .header-right
+    width: 1.24rem
+    float: right
+</style>
