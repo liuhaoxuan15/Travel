7d0b870fd1285172ef02191dc9340f1524668237
liuhaoxuan <liuhaoxuan@cascadiacorp.com>
2018-12-10 10:52:20 +0800

    修改

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        Home
+        <br>
+        <router-link to="/list" class="home">列表页</router-link>
+    </div>
+</template>
+<script>
+export default{
+  name: 'Home'
+}
+</script>
+<style>
+.home{
+font-size: 50px;
+}
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+        List
+    </div>
+</template>
+<script>
+export default{
+  name: 'List'
+}
+</script>
+<style>
+.home{
+  font-size: 50px;
+}
+</style>
