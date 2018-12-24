ef4c97c9dd16beb650e845359eda7b1a147dd811
liuhaoxuan15 <703929634@qq.com>
2018-12-24 16:26:30 +0800

    header渐隐渐现

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <router-link v-show="showAbs" tag="div" to="/" class="header-abs">
-      <span class="iconfont header-abs-back">&#xe624;</span>
+    <router-link
+      tag="div"
+      to="/"
+      class="header-abs"
+      v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
     </router-link>
-    <div class="header-fixed" v-show="!showAbs">
-      <router-link to="/" class="header-abs">
+    <div
+      class="header-fixed"
+      v-show="!showAbs"
+      :style="opacityStyle"
+    >
+      <router-link to="/">
         <div class="iconfont header-fixed-back">&#xe624;</div>
       </router-link>
       景点详情
@@ -13,13 +21,34 @@
 </template>
 
 <script>
-
-export default{
+export default {
   name: 'DetailHeader',
   data () {
     return {
-      showAbs: true
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
     }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  unmounted () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
